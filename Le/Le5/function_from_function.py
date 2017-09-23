13b6bc26acc7caa581c990155500703126ff066a
Ioannis.Arailopoulos <ioannis.arailopoulos@arup.com>
2017-09-23 17:20:12 +0100

    Lecture 5 script finalised

--- Le/Le5/function_from_function.py
+++ Le/Le5/function_from_function.py
@@ -24,8 +24,43 @@ def make_adder(n):
         return k + n
     return adder
 
+def f(x, y):
+    return g(x)
+
+def g(a):
+    return a + y
+
+
 def apply_twice(f, x):
     return f(f(x))
 
 def square(x):
     return x * x
+
+def triple(x):
+    return 3 * x
+
+def compose1(f, g):
+    def h(x):
+        return f(g(x))
+    return h
+
+
+def repeat(f, x):
+    while f(x) != x:
+        x = f(x)
+    return x
+
+def g(y):
+    return (y + 5) // 3
+
+y = 20
+x = 3
+def f():
+    y = 2
+    return y, x
+
+
+
+x = 10
+squarelam = lambda x: x * x
