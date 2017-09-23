28f5c0fd545467fd015862b15ca20661c4156c80
Ioannis.Arailopoulos <ioannis.arailopoulos@arup.com>
2017-09-23 19:37:44 +0100

    Lecture no 6 done

--- Le/Le6/recursion.py
+++ Le/Le6/recursion.py
@@ -17,14 +17,12 @@ def split(n):
     """Sum the digits of N"""
     return n // 10, n % 10
 
-def summation(n):
+def sum_digits(n):
     if n < 10:
         return n
     else:
         all_but_last, last = split(n)
-        return summation(all_but_last) + last
-
-
+        return sum_digits(all_but_last) + last
 
 """
 Intermediate Recursion
@@ -44,3 +42,24 @@ A function that calls a function that calls this function
 
 """
 
+def luhn_sum(n):
+    if n < 10:
+        return n
+    else:
+        all_but_last, last = split(n)
+        return luhn_sum_double(all_but_last) + last
+
+def luhn_sum_double(n):
+    all_but_last, last = split(n)
+    luhn_digit = sum_digits(2 * last)
+    if n < 10:
+        return luhn_digit
+    else:
+        return luhn_sum(all_but_last) + luhn_digit
+
+
+def luhn_check(n):
+    if luhn_sum(n) % 10 == 0:
+        return True
+    else:
+        return False
